4a135de59f5d2a87ef027dbb7c2a8f399a4178ad
Borbi94 <a223205113@unison.mx>
2024-08-14 12:29:44 -0700

    Se sube proyecto.

--- src/main/java/postaleshmo.java
+++ src/main/java/postaleshmo.java
@@ -30,7 +30,6 @@ public class postaleshmo {
             System.out.println("Asentamientos rurales: " + rurales);
 
         } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
